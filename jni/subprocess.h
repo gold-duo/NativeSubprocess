9b3461cde4e61692cca82ca75c2d319f7915e549
droidwolf <droidwolf2013@gmail.com>
2014-08-20 10:03:39 +0800

    update package name

---
+++ jni/subprocess.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 droidwolf(droidwolf2010@gmail.com)
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+#include "jni.h"
+
+#ifndef SUBPROCESS_H_
+#define SUBPROCESS_H_
+
+
+static void JNICALL create (JNIEnv *env, jclass thiz, jobject context, jclass clazz);
+
+
+#endif /* SUBPROCESS_H_ */
